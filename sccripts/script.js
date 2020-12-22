2b1b2333178d81885b83ce1c678b7524bba7cb2e
Gadlevskiy <gadlevsky.alexei@yandex.ru>
2020-12-22 15:29:46 +0300

    feat: запрограммировать открытие/закрытие popup

--- sccripts/script.js
+++ sccripts/script.js
@@ -1,6 +1,29 @@
-let likeButton = document.querySelector('.elements__like-btn');
-console.log(likeButton)
-function likeswitch() {
-  likeButton.classList.toggle('.elements__like-btn_active')
+let likeButton = document.querySelectorAll('.elements__like-btn')
+
+for (var i=0; i < likeButton.length; i += 1) {
+  likeButton[i].addEventListener('click' , function() {
+    this.classList.toggle('elements__like-btn_active')
+})};
+
+let overlay = document.querySelector('.overlay')
+function TogglePopup() {
+  overlay.classList.toggle('overlay_active')
 }
-likeButton.addEventListener('click', likeswitch());
+
+let editButton = document.querySelector('.profile__btn-edit')
+let closeButton = document.querySelector('.popup__btn-close')
+
+editButton.addEventListener('click', event => {
+  event.preventDefault()
+  TogglePopup()
+})
+
+closeButton.addEventListener('click', TogglePopup)
+
+overlay.addEventListener('click', event => {
+  if (event.target === event.currentTarget) {
+    TogglePopup()
+  }
+})
+
+let form = overlay.querySelector('.popup__form')
