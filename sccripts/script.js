032da0c06845c845041b63a9b3202ed922c32b26
Gadlevskiy <gadlevsky.alexei@yandex.ru>
2020-12-24 08:36:06 +0300

    fix: исправить js функции

--- sccripts/script.js
+++ sccripts/script.js
@@ -1,44 +1,29 @@
-let likeButton = document.querySelectorAll('.elements__like-btn')
-
-for (var i=0; i < likeButton.length; i += 1) {
-  likeButton[i].addEventListener('click' , function() {
-    this.classList.toggle('elements__like-btn_active')
-})};
-
 let overlay = document.querySelector('.overlay')
-function TogglePopup() {
-  overlay.classList.toggle('overlay_active')
-}
-
 let editButton = document.querySelector('.profile__btn-edit')
-let addProfileButton = overlay.querySelector('.popup__btn-add-profile')
+let popupForm = overlay.querySelector('.popup__form')
 let closeButton = document.querySelector('.popup__btn-close')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
-let inputName = overlay.querySelector('.popup__input-name')
-let inputDescription = overlay.querySelector('.popup__input-description')
+let inputName = overlay.querySelector('.popup__input_type_name')
+let inputDescription = overlay.querySelector('.popup__input_type_description')
 
-editButton.addEventListener('click', event => {
-  event.preventDefault()
-  TogglePopup()
+function popupActive() {
+  overlay.classList.add('overlay_active')
   inputName.value = profileName.textContent
-  console.log(inputName.value)
   inputDescription.value = profileDescription.textContent
-})
+}
 
-addProfileButton.addEventListener('click', event => {
-  event.preventDefault()
+function popupDisable() {
+  overlay.classList.remove('overlay_active')
+}
+
+function popupSubmit(evt) {
+  evt.preventDefault()
   profileName.textContent = inputName.value
-  console.log(profileName.innerHTML)
   profileDescription.textContent = inputDescription.value
-  TogglePopup()
-})
+  popupDisable()
+}
 
-closeButton.addEventListener('click', TogglePopup)
-
-overlay.addEventListener('click', event => {
-  if (event.target === event.currentTarget) {
-    TogglePopup()
-  }
-})
-
+editButton.addEventListener('click', popupActive)
+closeButton.addEventListener('click', popupDisable)
+popupForm.addEventListener('submit', popupSubmit)
